401c452650926d5c58acae93b5a993603372d76b
Ann <melody6666@live.com>
2017-10-07 17:36:07 +0800

    Progression bar

---
+++ progress-bar/progression-bar.php
@@ -0,0 +1,17 @@
+<?php 
+    include('Conf/init.php');
+?>
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="CSS/progression-bar.css" />
+</head>
+<body>
+    <?php
+	   $result = mysqli_query($conn,"SELECT * FROM enrollment INNER JOIN lesson ON enrollment.LessonID = lesson.LessonID WHERE StudentID=2");
+		while($row=mysqli_fetch_array($result))
+		{ ?> 
+    <progress value="<?php echo $row['Status'];?>" max="<?php echo $row['TotalTopic'];?>">
+</progress>
+		<?php }?>
+</body>
+</html>
