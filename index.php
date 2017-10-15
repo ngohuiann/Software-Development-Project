676bd703ea5575787a9d1ce71a2ff8e8f1043021
Ngo <melody6666@live.com>
2017-10-15 22:15:39 +0800

    merged subject tab index.php and fixed size bug

--- index.php
+++ index.php
@@ -59,9 +59,9 @@ if($rowcount==1)
 ?>
 <head>
     <link rel="stylesheet" type="text/css" href="CSS/index.css"/>
-	<link href="https://fonts.googleapis.com/css?family=Dosis|Titillium+Web" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Dosis|Titillium Web" rel="stylesheet">
     <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Cuprum|Oswald|Overpass+Mono|Saira+Extra+Condensed" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Cuprum|Oswald|Overpass Mono|Saira Extra Condensed" rel="stylesheet">
 </head>
 <body>
 	
@@ -127,43 +127,78 @@ if($rowcount==1)
 			</div>
         </div>
     </div>
-    <div class="course-desc">
-        <div style="background-color:#eee;">
-		 <div class="subject-tab">
-			 <button class="subjectlinks htmltab" onclick="openSubject(event, 'HTML')">HTML</button>
-			<button class="subjectlinks csstab" onclick="openSubject(event, 'CSS')">CSS</button>
-			<button class="subjectlinks javascripttab" onclick="openSubject(event, 'Javascript')">Javascript</button>
-		 </div>
-        </div>
-		<div id="HTML" class="subjectcontent">
-			<h3>HTML</h3>
-			<p>bla.</p>
-		</div>
-
-		<div id="CSS" class="subjectcontent">
-		    <h3>CSS</h3>
-		    <p>bla.</p>
-		</div>
-
-		<div id="Javascript" class="subjectcontent">
-		    <h3>Javascript</h3>
-		    <p>bla.</p>
-		</div> 
-		
-		<div class="overview-tab">
-			 <button class="overviewlinks" onclick="openOverview(event, 'top')" id="defaultOpen">Top</button>
-			<button class="overviewlinks" onclick="openOverview(event, 'most')">Most</button>
-		 </div>
-		<div id="top" class="overviewcontent">
-			<h3>Top</h3>
-			<p>bla.</p>
-		</div>
-
-		<div id="most" class="overviewcontent">
-		    <h3>Most</h3>
-		    <p>bla.</p>
-		</div> 
-    </div>
+    <div class="course-desc" id="subjectbar">
+         <div style="background-color:#2b303b;">
+ 		 <div class="subject-tab">
+ 			 <button class="subjectlinks htmltab" onclick="openSubject(event, 'HTML')"><span>&lt;</span> HTML <span>&gt;</span></button>
+ 			<button class="subjectlinks csstab" onclick="openSubject(event, 'CSS')"><span>&lt;</span> CSS <span>&gt;</span></button>
+ 			<button class="subjectlinks javascripttab" onclick="openSubject(event, 'Javascript')"><span>&lt;</span> JavaScript <span>&gt;</span></button>
+ 		 </div>
+         </div>
+ 		<div id="HTML" class="subjectcontent">
+ 			<div class="container">
+                 <div class="moduletitle">
+                     <a>
+                         <h3>1&nbsp;&nbsp;HTML: Introduction to HTML</h3>
+                     </a>
+                 </div>
+                 <div class="moduletitle">
+                     <a>
+                         <h3>2&nbsp;&nbsp;HTML: Basic elements</h3>
+                     </a>
+                 </div>
+                 <div class="moduletitle">
+                     <a>
+                         <h3>3&nbsp;&nbsp;HTML: Attributes</h3>
+                     </a>
+                 </div>
+                 <a href="html.php" id="showall">Show All</a>
+             </div>
+         </div>
+             
+ 
+ 		<div id="CSS" class="subjectcontent">
+ 			<div class="container">
+                 <div class="moduletitle">
+                     <a>
+                         <h3>1&nbsp;&nbsp;CSS: Introduction to CSS</h3>
+                     </a>
+                 </div>
+                 <div class="moduletitle">
+                     <a>
+                         <h3>2&nbsp;&nbsp;CSS: Syntax</h3>
+                     </a>
+                 </div>
+                 <div class="moduletitle">
+                     <a>
+                         <h3>3&nbsp;&nbsp;CSS: Background &amp; Colors</h3>
+                     </a>
+                 </div>
+                 <a href="css.php" id="showall">Show All</a>
+             </div>
+         </div>
+ 
+ 		<div id="Javascript" class="subjectcontent">
+ 			<div class="container">
+                 <div class="moduletitle">
+                     <a>
+                         <h3>1&nbsp;&nbsp;JavaScript: Introduction to JavaScript</h3>
+                     </a>
+                 </div>
+                 <div class="moduletitle">
+                     <a>
+                         <h3>2&nbsp;&nbsp;JavaScript: Syntax</h3>
+                     </a>
+                 </div>
+                 <div class="moduletitle">
+                     <a>
+                         <h3>3&nbsp;&nbsp;JavaScript: Statements</h3>
+                     </a>
+                 </div>
+                 <a href="css.php" id="showall">Show All</a>
+             </div>
+         </div>
+     </div>
 	<div class="beforefooter">
     </div>
 	<script src="JS/index-overviewTab.js"></script>
